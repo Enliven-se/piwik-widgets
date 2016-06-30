1c6c37d0a2c0039728186e61af79488d41106c16
Satish Rajput <satish@caratred.com>
2016-06-30 17:28:31 +0530

    added animation to the chart

--- src/app/components/interaction/interactionRateGuage.js
+++ src/app/components/interaction/interactionRateGuage.js
@@ -19,14 +19,15 @@ const HeaderStyles = {
 let Gauge = React.createClass({
   getInitialState() {
     const state = {
-      getAPI: "http://demo.piwik.org/index.php?module=API&method=API.get&format=JSON&idSite=7&period=year&date=2016-05-30,2016-06-28&date=2016-05-30%2C2016-06-28&filter_limit=false&format_metrics=1&expanded=1&token_auth=anonymous&filter_limit=30",
+      baseAPI: "http://demo.piwik.org/index.php?",
+      interactionRateAPI: "module=API&method=API.get&format=JSON&idSite=7&period=year&date=2016-05-30,2016-06-28&date=2016-05-30%2C2016-06-28&filter_limit=false&format_metrics=1&expanded=1&token_auth=anonymous&filter_limit=30",
       dataR: ''
     };
     return state;
   },
 
   componentDidMount() {
-    this._interactionChart(this.state.getAPI);
+    this._interactionChart(this.state.baseAPI + this.state.interactionRateAPI);
   },
 
   _getLiveData(apiLink) {
@@ -36,7 +37,7 @@ let Gauge = React.createClass({
       cashe: false,
       crossDomain: true,
       success: function (data) {
-        console.log(`data received: ${JSON.stringify(data)}`);
+        console.log(`data received from api: ${apiLink} is \n ${JSON.stringify(data)}`);
         this.setState({
           dataR: data
         });
@@ -51,15 +52,49 @@ let Gauge = React.createClass({
     this._getLiveData(apiLink).done(function (data) {
       const interactionRate = ((data['2016'].nb_visits - data['2016'].bounce_count) / data['2016'].nb_visits * 100).toFixed(2);
       console.log(`interactionRate: ${interactionRate}%`);
-      c3.generate({
+      const interactionChart = c3.generate({
         bindto: '#chart_1',
         data: {
           columns: [
             ['data', interactionRate]
           ],
           type: 'gauge'
+        },
+        color: {
+          pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'],
+          threshold: {
+            values: [30, 60, 90, 100]
+          }
+        },
+        size: {
+          height: 180
         }
       });
+      setTimeout(() => {
+        interactionChart.load({
+          columns: [['data', 10]]
+        });
+      }, 1000);
+      setTimeout(() => {
+        interactionChart.load({
+          columns: [['data', 50]]
+        });
+      }, 2000);
+      setTimeout(() => {
+        interactionChart.load({
+          columns: [['data', 70]]
+        });
+      }, 3000);
+      setTimeout(() => {
+        interactionChart.load({
+          columns: [['data', 100]]
+        });
+      }, 4000);
+      setTimeout(() => {
+        interactionChart.load({
+          columns: [['data', interactionRate]]
+        });
+      }, 5000);
     });
   },
 
