f074d2f3ffeaf2c0fe8ebcedbcd15ae7dda07f96
Satish Rajput <satish@caratred.com>
2016-06-30 14:40:23 +0530

    api is externalised

--- src/app/components/interaction/interactionRateGuage.js
+++ src/app/components/interaction/interactionRateGuage.js
@@ -19,23 +19,28 @@ const HeaderStyles = {
 let Gauge = React.createClass({
   getInitialState() {
     const state = {
+      getAPI: "http://demo.piwik.org/index.php?module=API&method=API.get&format=JSON&idSite=7&period=year&date=2016-05-30,2016-06-28&date=2016-05-30%2C2016-06-28&filter_limit=false&format_metrics=1&expanded=1&token_auth=anonymous&filter_limit=30",
       bounceRate: '',
       visits: '',
       bounceCount: '',
       interactionCount: '',
       dataR: ''
     };
-
     return state;
   },
 
   componentDidMount() {
+    this._getLiveData(this.state.getAPI);
+  },
+
+  _getLiveData(apiLink) {
     $.ajax({
-      url: "http://demo.piwik.org/index.php?module=API&method=API.get&format=JSON&idSite=7&period=year&date=2016-05-30,2016-06-28&date=2016-05-30%2C2016-06-28&filter_limit=false&format_metrics=1&expanded=1&token_auth=anonymous&filter_limit=30",
+      url: apiLink,
       dataType: 'jsonp',
       cashe: false,
       crossDomain: true,
       success: function (data) {
+        console.log(`data received: ${JSON.stringify(data)}`);
         this.setState({
           dataR: data,
           bounceRate: data['2016'].bounce_rate,
@@ -44,8 +49,8 @@ let Gauge = React.createClass({
           interactionCount: data['2016'].nb_visits - data['2016'].bounce_count
         });
         const interactionRate = (this.state.interactionCount / this.state.visits * 100).toFixed(2);
-        console.log('interactionRate: ${interactionRate}%');
-        this._renderChart(interactionRate);
+        console.log(`interactionRate: ${interactionRate}%`);
+        this._interactionChart(interactionRate);
       }.bind(this),
       error: function (xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -53,7 +58,7 @@ let Gauge = React.createClass({
     });
   },
 
-  _renderChart(interactionRate) {
+  _interactionChart(interactionRate) {
     const lineChart = c3.generate({
       bindto: '#chart_1',
       data: {
@@ -64,6 +69,7 @@ let Gauge = React.createClass({
       }
     });
   },
+
   render() {
     return (
       <div id="chart_1"></div>
@@ -72,12 +78,6 @@ let Gauge = React.createClass({
 });
 
 class InteractionRateGuage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      piwikApi1: "http://demo.piwik.org/index.php?module=API&method=API.get&format=JSON&idSite=7&period=year&date=2016-05-30,2016-06-28&date=2016-05-30%2C2016-06-28&filter_limit=false&format_metrics=1&expanded=1&token_auth=anonymous&filter_limit=30"
-    };
-  }
   render() {
     return (
       <div>
