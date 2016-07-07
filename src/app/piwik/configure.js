834b92cb9067be910a3aab81b28b7b0b70714c3a
Satish Rajput <satish@caratred.com>
2016-07-07 16:21:12 +0530

    add configure file to gitignore

--- src/app/piwik/configure.js
+++
@@ -1,5 +0,0 @@
-// put this file at "src/piwik/configure.js"
-function Configuration() {
-  this.baseAPI = "http://demo.piwik.org/index.php?module=API&format=JSON&token_auth=";
-  this.authtoken = "anonymous";
-}
