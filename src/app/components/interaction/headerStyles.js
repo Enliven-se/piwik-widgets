5403941691a7aebf80864c8808ee1476c18b5110
Satish Rajput <satish@caratred.com>
2016-07-07 16:15:24 +0530

    separated out styling

---
+++ src/app/components/interaction/headerStyles.js
@@ -0,0 +1,21 @@
+const HeaderStyles = {
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#1f1f1f'
+  },
+  date: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: '1.5rem',
+    margin: '1rem',
+    color: 'white'
+  },
+  charts: {
+    flex: 1,
+    textAlign: 'left',
+    fontSize: '1rem',
+    margin: '1rem',
+    align: 'left'
+  }
+};
--- src/app/piwik/configure.js
+++ src/app/piwik/configure.js
@@ -1,3 +1,4 @@
+// put this file at "src/piwik/configure.js"
 function Configuration() {
   this.baseAPI = "http://demo.piwik.org/index.php?module=API&format=JSON&token_auth=";
   this.authtoken = "anonymous";
